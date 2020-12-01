cfebe849189d0a0def2e0532292d9415212328e8
kojima <hide.k0712@gmail.com>
2020-12-01 11:27:28 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
-})
+//% weight=70 color=#e67e22 icon="\uf005" block="津田小5年ブロック"
+namespace comment {
+    //% blockId=test_block block="メッセージ表示 %v"
+    export function showmessage(text: string): void {
+    }
+}
