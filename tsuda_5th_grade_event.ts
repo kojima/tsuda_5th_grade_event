93163a206a8b6bfe395775369dbba2cf4ebfa879
kojima <hide.k0712@gmail.com>
2020-12-01 12:15:35 +0900

    Add a file for the custom blocks

---
+++ tsuda_5th_grade_event.ts
@@ -0,0 +1,6 @@
+//% weight=70 color=#e67e22 icon="\uf005" block="津田小5年ブロック"
+namespace tsuda_5th_grade_event {
+    //% blockId=test_block block="メッセージ表示 %v"
+    export function showmessage(text: string): void {
+    }
+}
