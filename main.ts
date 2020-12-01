50e97c771b6e67f1141b62aacc690f0a68e0be36
kojima <hide.k0712@gmail.com>
2020-12-01 11:27:58 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 //% weight=70 color=#e67e22 icon="\uf005" block="津田小5年ブロック"
-namespace comment {
+namespace tsuda_5th_grade_event {
     //% blockId=test_block block="メッセージ表示 %v"
     export function showmessage(text: string): void {
     }
